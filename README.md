e2f29d8c6cce761bae4e9459de62a9dbe07ec70b
Samuel Carnell <samuel.carnell@outlook.com>
2022-04-09 14:12:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project-euler-rust
+ My solutions to Project Euler problems in the Rust programming language.
---
+++ src/p1_100/p1.rs
@@ -0,0 +1,21 @@
+// The nth triangular number.
+pub fn triangle(n: u32) -> u32 {
+    n * (n + 1) / 2
+}
+
+/// The number of multiples of n less than limit.
+pub fn multiple_count(n: u32, limit: u32) -> u32 {
+    limit / n
+}
+
+/// The sum of all multiples of n less than limit.
+pub fn multiple_sum(n: u32, limit: u32) -> u32 {
+    n * triangle(multiple_count(n, limit))
+}
+
+pub fn p() {
+    const LIMIT: u32 = 1000 - 1;
+
+    let ans = multiple_sum(5, LIMIT) + multiple_sum(3, LIMIT) - multiple_sum(15, LIMIT);
+    println!("answer: {ans}") // answer: 233168
+}
